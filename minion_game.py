df954ab2d19f651d848402e948a430b8f5411c4d
QaziPython <rafehqazi1@gmail.com>
2016-09-19 11:11:51 -0500

    perms_with_consonant is complete. Just have to clean it up now

--- minion_game.py
+++ minion_game.py
@@ -8,6 +8,19 @@ from collections import namedtuple
 
 
 def count_occurrence(text='', pattern=''):
+    """
+    Count how many times a pattern is found inside of a text.
+    :param text: str
+    :param pattern: str
+    :return: int
+
+    >>> count_occurrence('banana', 'b')
+    1
+    >>> count_occurrence('banana', 'ana')
+    2
+    >>> count_occurrence('banana', 'a')
+    3
+    """
     match = 0
     for i in range(len(text)):
         for j in range(len(pattern)):
@@ -18,41 +31,6 @@ def count_occurrence(text='', pattern=''):
                     match += 1
                     break
     return match
-
-
-print(count_occurrence('banana', 'b'))
-
-
-def perms_with_consonant(word):
-    word_scores = []
-    Consonant = namedtuple('Consonant', ['position', 'letter'])
-    word = word.lower()
-    consonants = [Consonant(index, letter) for index, letter in enumerate(word) if
-                  letter not in ('a', 'e', 'i', 'o', 'u')]
-    print(consonants)
-    for consonant in consonants:
-        print(consonant.position, consonant.letter)
-        if not consonant.position == len(word) - 1:
-            # Skip the letter
-            new_word = word[:consonant.position] + word[consonant.position + 1:]
-            perms = permutations(new_word)
-            for perm in perms:
-                print(''.join(perm))
-                found = count_occurrence(word, ''.join(perm))
-                print(found)
-                if found:
-                    word_scores.append((''.join(perm), found))
-            print(new_word)
-        else:
-            new_word = word[:-1]
-            print(new_word)
-    # results = []
-    #
-    # for perm in perms:
-    #     results.append(consonants[0] + ''.join(perm))
-
-    # return results
-    print(word_scores)
 
 
 def all_length_perms(word):
@@ -70,9 +48,37 @@ def all_length_perms(word):
     return perms
 
 
+def perms_with_consonant(word):
+    word_scores = set()
+    Consonant = namedtuple('Consonant', ['position', 'letter'])
+    word = word.lower()
+    consonants = [Consonant(index, letter) for index, letter in enumerate(word) if
+                  letter not in ('a', 'e', 'i', 'o', 'u')]
+    print(consonants)
+    for consonant in consonants:
+        print(consonant.position, consonant.letter)
+        if not consonant.position == len(word) - 1:
+            # Skip the letter
+            new_word = word[:consonant.position] + word[consonant.position + 1:]
+            perms = all_length_perms(new_word)
+            for perm in perms:
+                print(perm)
+                found = count_occurrence(word, perm)
+                print(found)
+                if found:
+                    word_scores.add((perm, found))
+            print(new_word)
+        else:
+            new_word = word[:-1]
+            print(new_word)
+
+    print(word_scores)
+    return sum((score for word, score in word_scores))
+
+print(perms_with_consonant('banana'))
+
 if __name__ == '__main__':
     import doctest
 
     doctest.testmod()
-
     print(all_length_perms('OBIAS'))
