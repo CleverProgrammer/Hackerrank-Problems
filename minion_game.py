cbd58c608de5b9d91c69339d8666219a168f87fb
QaziPython <rafehqazi1@gmail.com>
2016-09-29 11:25:49 -0500

    Complete minion game.

--- minion_game.py
+++ minion_game.py
@@ -2,97 +2,56 @@
 Author: Rafeh Qazi
 Program: Minion Game (Hackerrank Challenge)
 Modified: September 13
+Submit this code at: https://www.hackerrank.com/challenges/the-minion-game
 """
-from itertools import permutations
 
 
-def count_occurrence(text='', pattern=''):
+def match(text, pattern):
     """
-    Count how many times a pattern is found inside of a text.
-    :param text: str
-    :param pattern: str
-    :return: int
-
-    >>> count_occurrence('banana', 'b')
-    1
-    >>> count_occurrence('banana', 'ana')
+    >>> match('banana', 'ana')
     2
-    >>> count_occurrence('banana', 'a')
-    3
     """
-    match = 0
-    for i in range(len(text)):
-        for j in range(len(pattern)):
-            if i + j < len(text):
-                if text[i + j] != pattern[j]:
-                    break
-                elif j == len(pattern) - 1:
-                    match += 1
-                    break
-    return match
+    return sum([1 for i in range(len(text)) if text[i:i + len(pattern)] == pattern])
 
 
-def all_length_perms(word):
+def substrings(word):
     """
-    Given a word, generate all length permutations.
-    :param word: str
-    :return: list
+    >>> substrings('hello')
+    ['h', 'he', 'hel', 'hell', 'hello']
+    """
+    return [word[:i + 1] for i in reversed(range(len(word)))]
 
-    >>> all_length_perms('ab')
-    ['a', 'b', 'ab', 'ba']
+
+def all_substrings(word):
     """
-    perms = []
-    for i in range(1, len(word) + 1):
-        perms += [''.join(perm) for perm in permutations(word, i)]
-    return perms
+    >>> all_substrings('hel')
+    [['h', 'he', 'hel'], ['e', 'el'], ['l']]
+    """
+    all_subs = []
+    for i in range(len(word)):
+        all_subs += substrings(word[i:])
+    return set(all_subs)
 
 
 def minion(word):
-    """
-    Create all permutations of the given word starting with its consonants.
-    Give 1 point for each permutation found as a substring in the given word.
-    Return score for player 1 and score for player 2. Player 1 is Stuart and
-    Player 2 is Kevin. Stuart has the consonants and Kevin the vowels
-    :param word: str
-    :return: tuple
+    stuart = 0
+    kevin = 0
+    consonants = 'bcdfghjklmnpqrstvwxyz' + 'bcdfghjklmnpqrstvwxyz'.upper()
+    vowels = 'aeiou' + 'aeiou'.upper()
 
-    >>> minion('banana')
-    'Stuart 12'
+    for string in all_substrings(word):
+        if string[0] in consonants:
+            stuart += match(word, string)
 
-    >>> minion('aaa')
-    'Kevin 6'
+        elif string[0] in vowels:
+            kevin += match(word, string)
 
-    >>> minion('ab')
-    'Kevin 2'
-    """
-    word = word.lower()
-    consonant = set('bcdfghjklmnpqrstvwxyz')
-    vowels = set('aeiou')
-    assert len(consonant.union(vowels)) == 26
-    perms = set(all_length_perms(word))
-
-    all_perms = set()
-
-    for perm in perms:
-        found = count_occurrence(word, perm)
-        if found and perm[0] in consonant:
-            all_perms.add((perm, found, 'consonant'))
-        elif found and perm[0] in vowels:
-            all_perms.add((perm, found, 'vowel'))
-
-    consonant_score = sum([score for _, score, type in all_perms if type == 'consonant'])
-    vowel_score = sum([score for _, score, type in all_perms if type == 'vowel'])
-
-    if consonant_score > vowel_score:
-        return 'Stuart {}'.format(consonant_score)
-
-    elif consonant_score == vowel_score:
+    if stuart > kevin:
+        return 'Stuart {}'.format(stuart)
+    elif stuart < kevin:
+        return 'Kevin {}'.format(kevin)
+    else:
         return 'Draw'
 
-    return 'Kevin {}'.format(vowel_score)
 
-
-if __name__ == '__main__':
-    import doctest
-
-    doctest.testmod()
+print(minion(input()))
